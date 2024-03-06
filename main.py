780a96f8e9e14c321b8b5764c1652244ea3f3115
Simon Kellegher <github@syborg.co.uk>
2024-03-06 09:06:28 +0100

    Working on finding the nearest town to any town.

--- main.py
+++ main.py
@@ -6,30 +6,46 @@ from geopy import distance
 
 
 def coords_as_ints(coords_string):
-    lat_lon = [x.strip() for x in coords_string.split(',')]
-    lat = lat_lon[0].lstrip('"')
-    lon = lat_lon[1].rstrip('"')
-    lat = float(lat)
-    lon = float(lon)
-    return lat, lon
+    try:
+        lat_lon = [x.strip() for x in coords_string.split(',')]
+        lat = lat_lon[0].lstrip('"')
+        lon = lat_lon[1].rstrip('"')
+        lat = float(lat)
+        lon = float(lon)
+        return lat, lon
+    except ValueError:
+        print(f"error: {coords_string}")
+    return 0, 0
 
 
 def open_file_and_copy_lines():
     text_input = open("Groningen Towns.txt")
     places_dict = {}
     for line in text_input:
-        line = [x.strip() for x in line.split('\t')]
-        places_dict[line[0]] = line[1]
+        try:
+            line = [x.strip() for x in line.split('\t')]
+            print(line[0])
+            places_dict[line[0]] = line[1]
+        except IndexError:
+            print(line[0])
+            places_dict[line[0]] = line[1]
+    print(len(places_dict))
     return places_dict
 
+def find_the_nearest(town_1, town_dict):
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     towns = open_file_and_copy_lines()
+    count = 0
     for town in towns:
+        count += 1
         coords = coords_as_ints(towns[town])
-        print(coords)
-
+        # print(f"{count}: {coords}")
+    print(len(towns))
+    town_a = coords_as_ints(towns["Groningen"])
+    town_b = coords_as_ints(towns["Ter Apel"])
     print(distance.distance(town_a, town_b))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
