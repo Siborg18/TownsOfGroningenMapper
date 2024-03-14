9a6d0597e93bbda484ea535f2b4763e3b4330cd5
Simon Kellegher <github@syborg.co.uk>
2024-03-14 09:34:25 +0100

    Exploring anytree to help manage the varies routes the algorithm could take.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import math
 
 import matplotlib.pyplot as plt
-import os
-from matplotlib import transforms
-import numpy as np
 from geopy import distance
+import requests
+from anytree import Node, RenderTree
 
+HTTP_SERVER = 'http://router.project-osrm.org'
+test_coordinates = 53.308333, 6.369444
 fig, ax = plt.subplots()
+
+
+def route_request():
+    # request_string ='http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false'
+    request_string = f"{HTTP_SERVER}" + "/nearest/v1/bike/6.369444,53.308333?"
+    r = requests.get(request_string)
+    print(r.status_code)
+    print(r.text)
+
+    # /route/v1/{profile}/{coordinates}?alternatives={true|false|number}&steps={true|false}&geometries={polyline|polyline6|geojson}&overview={full|simplified|false}&annotations={true|false}
 
 
 def open_file_and_copy_lines():
@@ -46,6 +57,7 @@ def find_towns_within_distance(origin_coords, towns_dict, distance_limit):
             towns_in_range.append((town, current_distance))
     print(f"Towns within {distance_limit}km of {origin_name}: {len(towns_in_range)}")
     print(towns_in_range)
+    return len(towns_in_range)
 
 
 def find_the_nearest(origin_coords, towns_dict):
@@ -84,6 +96,16 @@ def find_neighbours(origin_coords, number_of_neighbours, towns_dict):
         i += 1
     return output_neighbours
 
+def find_neighours_closer_to_target(current_town, target_town, number_of_neighbours, towns_dict):
+    current_neighbours = find_neighbours(current_town, number_of_neighbours, towns_dict)
+    closer_neighbours = []
+    distance_without_deviation = distance.distance(current_town, target_town)
+    for i in range(0, len(current_neighbours)):
+        if distance.distance(current_neighbours[i], target_town) < distance_without_deviation:
+            graph_completed_green(current_neighbours[i])
+            closer_neighbours.append(current_neighbours[i])
+            print(current_neighbours[i])
+    return closer_neighbours
 
 def find_the_furthest(origin_coords, towns_dict):
     distance_furthest_town = 0
@@ -91,7 +113,6 @@ def find_the_furthest(origin_coords, towns_dict):
         current_distance = distance.distance(origin_coords, towns_dict[town])
         if current_distance == 0:
             origin_name = town
-            print("Do nothing")
         elif current_distance > distance_furthest_town:
             distance_furthest_town = current_distance
             name_of_town = town
@@ -103,9 +124,11 @@ def graph_completed_green(town_completed):
     x, y = town_completed[1], town_completed[0]
     ax.scatter(x, y, color="green")
 
+
 def graph_target_red(town):
     x, y = town[1], town[0]
     ax.scatter(x, y, color="red")
+
 
 def graph_towns(*town_to_graph, towns_dict):
     groningen = towns_dict["Groningen"]
@@ -118,62 +141,38 @@ def graph_towns(*town_to_graph, towns_dict):
         # print(my_town)
 
 
-def Dijkstra_algorithm(origin_town, target_town):
+def dijkstra_algorithm(origin_town, target_town, towns_dict):
+    my_root_of_tree = Node(origin_town)
+
+    current_neighbours = find_neighours_closer_to_target(origin_town, target_town, 5, towns_dict)
+    for i in range(0, len(current_neighbours)):
+        my_tree_children = Node(current_neighbours[i], parent=my_root_of_tree)
+    print(RenderTree(my_root_of_tree))
     # Find distance between two towns without any deviation
     # Find neighbours of origin town
     # Find if distance from neighbour is greater than origin
-
     pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # route_request()
     towns_dict = open_file_and_copy_lines()
     print(distance.distance(towns_dict["Stadskanaal"], towns_dict["Lauwersoog"]))
 
     for town in towns_dict:
         graph_towns(towns_dict[town], towns_dict=towns_dict)
 
-    test_town = towns_dict["Schildwolde"]
-    find_the_nearest(test_town, towns_dict)
-    find_the_furthest(test_town, towns_dict)
-    # test_town_2 = coords_as_ints(towns["Groningen"])
-    find_towns_within_distance(test_town, towns_dict, 5)
+    test_town = towns_dict["Groningen"]
+    test_town_b = towns_dict["Ter Apel"]
+
+    dijkstra_algorithm(test_town, test_town_b, towns_dict)
+
+    towns_within_distance = find_towns_within_distance(test_town, towns_dict, 1)
     # graph_completed_green(test_town)
     #
-    my_neighbours = find_neighbours(test_town, 10, towns_dict)
+    my_neighbours = find_neighbours(test_town, towns_within_distance, towns_dict)
     graph_target_red(test_town)
-    for neighbour in my_neighbours:
-        graph_completed_green(neighbour)
-    #
-    # lauwersoog = coords_as_ints(towns["Lauwersoog"])
-    # Delfzijl = coords_as_ints(towns["Delfzijl"])
-    # ter_apel = coords_as_ints(towns["Ter Apel"])
-    # graph_towns(groningen, ter_apel, zoutkamp)
-    # find_the_nearest(origin_coords=groningen, town_dict=towns)
-    # find_the_furthest(origin_coords=groningen, town_dict=towns)
-    # x = lauwersoog[0]
-    # y = lauwersoog[1]
-    # my_color = "green"
-    # fig, ax = plt.subplots()
-    # my_distance = distance.distance(lauwersoog, Delfzijl)
-    # print(f"Distance from Groningen to Ter Apel: {my_distance}")
-    # my_distance = distance.distance(lauwersoog, ter_apel)
-    # print(f"Distance from Groningen to Zoutkamp: {my_distance}")
-    # my_distance = distance.distance(ter_apel, Delfzijl)
-    # print(f"Distance from Zoutkamp to Ter Apel: {my_distance}")
-    # ax.scatter(x, y, color=my_color)
-    # print(f"Groningen: {x}, {y}")
-    # x = Delfzijl[0]
-    # y = Delfzijl[1]
-    # my_color = "red"
-    # ax.scatter(x, y, color=my_color)
-    # print(f"Ter Apel: {x}, {y}")
-    # x = ter_apel[0]
-    # y = ter_apel[1]
-    # my_color = "orange"
-    # ax.scatter(x, y, color=my_color)
-    # print(f"Zoutkamp: {x}, {y}")
 
     plt.show()
 
