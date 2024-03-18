7c246ea1bf6b8c76d7186d344b7cb3b0050b4570
Simon Kellegher <github@syborg.co.uk>
2024-03-18 09:35:39 +0100

    Working on inputting location coords into GPX friendly string

--- OSRM_map_route.py
+++ OSRM_map_route.py
@@ -2,12 +2,22 @@ import requests
 import json
 
 HTTP_SERVER = 'http://router.project-osrm.org'
-test_coords = "6.566667,53.219444"
+test_coord_a = "53.219444, 6.566667"
+test_coord_b = "53.234860359373265, 6.548037763963784"
 
-def route_request():
+
+def coords_lon_lat_no_spaces(coords):
+    coords_as_list = [x.strip() for x in coords.split(',')]
+    coords_as_list.reverse()
+    coords_as_string = ','.join(coords_as_list)
+    print(coords_as_string)
+    return coords_as_string
+
+
+def route_request(route_coords):
     # request_string ='http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407;13.428555,52.523219?overview=false'
-    request_string = f"{HTTP_SERVER}/nearest/v1/bike/{test_coords}?"
-    #request_string = f'{HTTP_SERVER}/route/v1/bike/{test_coords};7.09161,53.174945?overview=false&annotations=true&steps=true'
+    # request_string = f"{HTTP_SERVER}/nearest/v1/bike/{route_coords}?"
+    request_string = f'{HTTP_SERVER}/route/v1/bike/{route_coords}?overview=false&annotations=true&steps=true'
     r = requests.get(request_string)
     print(r.text)
     my_json = json.loads(r.text)
@@ -15,4 +25,6 @@ def route_request():
 
     # /route/v1/{profile}/{coordinates}?alternatives={true|false|number}&steps={true|false}&geometries={polyline|polyline6|geojson}&overview={full|simplified|false}&annotations={true|false}
 
-route_request()
+
+my_coords = f"{coords_lon_lat_no_spaces(test_coord_a)};{coords_lon_lat_no_spaces(test_coord_b)}"
+route_request(my_coords)
